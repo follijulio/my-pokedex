356880de45c30205d303557416d2d62a5e8f1e1a
follijulio <follijulio@gmail.com>
2025-04-06 23:10:03 -0300

    atualiza README.md e adiciona imagem e favicon; ajusta interface CardPokemonProps

--- src/ui/cards/CardPokemon.tsx
+++ src/ui/cards/CardPokemon.tsx
@@ -6,8 +6,7 @@ interface CardPokemonProps {
 export const CardPokemon: React.FC<CardPokemonProps> = ({ image, name }) => {
   return (
     <div className="flex flex-col items-center justify-center w-40 h-52 bg-gradient-to-b from-purple-900 to-gray-950 rounded-2xl shadow-md hover:shadow-2xl hover:scale-105 transition-all duration-300 border border-white">
-      <img src={image} alt={name} className="w-24 h-24 mb-2 object-cover" />
-      <h2 className="text-lg font-semibold text-white">{name}</h2>
+      <img src={image} alt={name} className="w-24 h-24 mb-2 object-cover" /><h2 className="text-lg font-semibold text-white">{name}</h2>
     </div>
   );
 };
